7b2bb9aaba3a7c2469147fd7c5198f0fe5e3a039
Lama <lama.in.pijamas@gmail.com>
2024-08-20 12:27:47 +0200

    Some styling

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   get "stats" => "system_stats#index"
+  root 'pages#index'
 
   # Defines the root path route ("/")
   # root "posts#index"
